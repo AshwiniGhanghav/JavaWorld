8600651793b3d3940370a5ba033044d6571b824f
AshwiniGhanghav <87668235+AshwiniGhanghav@users.noreply.github.com>
2021-11-07 21:53:48 -0800

    Update JavaFirstProgram.java

--- src/main/java/JavaFirstProgram.java
+++ src/main/java/JavaFirstProgram.java
@@ -3,6 +3,6 @@ public class JavaFirstProgram {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World Ashwini !!!");
-		System.out.println("Thank u so much !!!");
+		System.out.println("Thank u so much pipeline!!!");
 	}
 }
